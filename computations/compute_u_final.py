4f0e94ae2e34ed3ca458535d053b328e07a9c798
bbeldycki <beldyckibartosz@gmail.com>
2022-10-10 00:28:48 +0200

    Refactor ComputeUFinal class

--- computations/compute_u_final.py
+++ computations/compute_u_final.py
@@ -1,5 +1,4 @@
-from typing import Optional, Dict
-
+from typing import Any
 import numpy as np
 
 
@@ -11,33 +10,17 @@ class ComputeUFinal:
         {'spin', 'u_start', 'mu_start', 'mu_final', 'angular_momentum', 'carter_c', 'mu_turning_points'}
         Output will be return in for of a dict() as well
     """
-    def __init__(self, input_variables: dict):
+    def __init__(self, input_variables: dict[str, Any]):
         self.input_variables = input_variables
-        self.temporary_variables = {'mu_integral_case': 0}
         self.output = dict()
         # initialize output with Nones
         self.setup_output_dict()
 
-        # set the coefficients for quadratic equation U(u)
-        self.c_coefficient = self.input_variables['spin'] ** 2.0 - self.input_variables['angular_momentum'] ** 2.0 \
-                             - self.input_variables['carter_c']
-        self.d_coefficient = 2.0 * ((self.input_variables['spin'] - self.input_variables['angular_momentum']) ** 2.0
-                                    + self.input_variables['carter_c'])
-        self.e_coefficient = -1.0 * self.input_variables['spin'] ** 2.0 * self.input_variables['carter_c']
-
     def setup_output_dict(self) -> None:
         self.output['u_final'] = None
         self.output['mu_integral'] = None
-
-    def compute(self) -> dict:
-        # we check for edge cases first
-        # edge case 1: carter constant = 0
-        if self.input_variables['carter_c'] == 0.0:
-            if self.input_variables['carter_c'] ** 2.0 >= self.input_variables['spin'] ** 2.0 or \
-                    self.input_variables['mu_start'] == 0.0:
-                self.update_out_case_0()
-                return self.output
-            self.temporary_variables['mu_integral_case'] = 1
+        self.output['case'] = None
+        self.output['u_turning_points'] = None
 
     def compute_mu_integral(self, case: int) -> float:
         def compute_case_1() -> float:
@@ -88,10 +71,51 @@ class ComputeUFinal:
                         'We checked all possible cases and did not return a proper value. Check file compute_u_final, '
                         'line: 42.')
 
-    def update_out_case_0(self):
-        self.output['mu_integral'] = 0.0
-        self.output['u_final'] = -1.0
-        self.output['case'] = 0
+    def update_output(self, case: int) -> None:
+        if case == 1:
+            self.output['mu_integral'] = 0.0
+            self.output['u_final'] = -1.0
+            self.output['case'] = 0
+        if case == 2:
+            self.output['u_final'] = -1.0
+            self.output['case'] = 4
+            self.output['u_turning_points'] = 0
+
+    def compute(self) -> dict[str, Any]:
+        # we check for edge cases first
+        # edge case 1: carter constant = 0
+        if self.input_variables['carter_c'] == 0.0:
+            if self.input_variables['carter_c'] ** 2.0 >= self.input_variables['spin'] ** 2.0 or \
+                    self.input_variables['mu_start'] == 0.0:
+                self.update_output(1)
+                return self.output
+            # we are going to compute mu integral case: 1
+            self.output['mu_integral'] = self.compute_mu_integral(1)
+        # edge case 2: spin = 0
+        if self.input_variables['spin'] == 0.0:
+            # we are going to compute mu integral case: 2
+            self.output['mu_integral'] = self.compute_mu_integral(2)
+        # create a list of coefficients in U(u) equation
+        # c_coefficient = coefficient_list[0]
+        # d_coefficient = coefficient_list[1]
+        # e_coefficient = coefficient_list[2]
+        coefficient_list = [
+            self.input_variables['spin'] ** 2.0 - self.input_variables['angular_momentum'] ** 2.0
+            - self.input_variables['carter_c'],
+            2.0 * ((self.input_variables['spin'] - self.input_variables['angular_momentum']) ** 2.0
+                   + self.input_variables['carter_c']),
+            -1.0 * self.input_variables['spin'] ** 2.0 * self.input_variables['carter_c']
+        ]
+        # edge case 3: carter constant = 0, spin = angular momentum and mu_start = 0
+        if coefficient_list[1] == coefficient_list[2] == 0.0:
+            self.update_output(2)
+            return self.output
+        # after basic edge cases we will solve equation U(u)
+        # we have two cases here:
+        # 1) U(u) is a third order polynomial
+        # 2) U(u) is a fourth order polynomial
+
+        # U(u) is a third order polynomial
 
 
 if __name__ == '__main__':
